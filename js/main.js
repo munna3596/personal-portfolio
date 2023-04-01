8be971cd489b0ec4a62282af436787c6bb7528bf
Your Name01~ <you@example.com>
2023-04-01 10:34:14 -0700

    portfolio all code done

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+window.addEventListener("scroll",function(){
+var header =this.document.querySelector("header");
+header.classList.toggle("sticky",window.scrollY > 0);
+})
+
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
